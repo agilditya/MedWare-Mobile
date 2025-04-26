6af75c8468c54597be4b9ab6c94cd639f27f78f3
agil <agilditya1004@gmail.com>
2025-04-26 17:16:20 +0700

    commit kesekian kali nya

--- lib/Admin/homeAdmin.dart
+++ lib/Admin/homeAdmin.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class MedwareHomePage extends StatelessWidget {
+class HomeAdmin extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'dashboard.dart';
+import 'User/profile.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      home: DashboardScreen(),
+      home: LogoutStaffPage(),
     );
   }
 }
